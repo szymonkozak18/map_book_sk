946fb34c7c1681a88549997c0d037948d8c5195b
szymonkozak18 <szymon.kozak18@wp.pl>
2025-04-15 17:27:37 +0200

    chore: the code was restructured according to the mvc template

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+from utils.model import users
+from utils.contoroller import gets_user_info
 
 
-users:list=[
-    {'name' :'maciej','location':'Łódź','posts':100},
-    {'name' :'mateusz','location':'Poznań','posts':200},
-    {'name' :'maciej01','location':'Siedlce','posts':300},
-    {'name' :'konrad','location':'Grudziądz','posts':400}
-]
+def main():
+    gets_user_info(users)
 
 
-def gets_user_info(users_data:list)->None:
-    for user in users_data:
-        print(f'twój znajomy {user['name']} z miejscowości {user["location"]} opublikował {user["posts"]} postów')
-
-gets_user_info(users)
+if __name__ == '__main__':
+    main()
---
+++ utils/model.py
@@ -0,0 +1,6 @@
+users: list = [
+    {'name': 'maciej', 'location': 'Łódź', 'posts': 100},
+    {'name': 'mateusz', 'location': 'Poznań', 'posts': 200},
+    {'name': 'maciej01', 'location': 'Siedlce', 'posts': 300},
+    {'name': 'konrad', 'location': 'Grudziądz', 'posts': 400}
+]
