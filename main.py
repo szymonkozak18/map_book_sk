c8154fbc3ddfec9833826b623377171e96749c2c
szymonkozak18 <szymon.kozak18@wp.pl>
2025-04-15 17:53:40 +0200

    wip: add MENU

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from utils.contoroller import gets_user_info
 
 
 def main():
-    gets_user_info(users)
+    print('===========MENU=============')
+    print('0 - zakończ program')
+    print('1 - wyświetl co u znajomych')
+    print('============================')
+    while True:
+        choice: str = input('wybierz opcję MENU: ')
+        if choice == '0': break
+        if choice == '1': gets_user_info(users)
 
 
 if __name__ == '__main__':
