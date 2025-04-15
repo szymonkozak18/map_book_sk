633c2317baf4bb4a74cab6b84d17c3510820156f
szymonkozak18 <szymon.kozak18@wp.pl>
2025-04-15 19:05:17 +0200

    wip: remove users

--- utils/contoroller.py
+++ utils/contoroller.py
@@ -1,3 +1,17 @@
 def gets_user_info(users_data: list) -> None:
     for user in users_data:
         print(f'twój znajomy {user['name']} z miejscowości {user["location"]} opublikował {user["posts"]} postów')
+
+
+def add_user(users_data: str) -> None:
+    new_name: str = input('podaj imię nowego znajomego: ')
+    new_location: str = input('podaj lokalizację: ')
+    new_posts: str = input('podaj liczbę postów: ')
+    users_data.append({'name': new_name, 'location': new_location, 'posts': new_posts}, )
+
+
+def remove_user(users_data: list) -> None:
+    user_name: str = input('podaj znajomego do usunięcia: ')
+    for user in users_data:
+        if user['name'] == user_name:
+            users_data.remove(user)
